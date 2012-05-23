e4ef01262501f257c370138f8caba96fda9f765a
Nate Ferrero <nateferrero@gmail.com>
2012-05-23 14:36:03 -0600

    whatever readme example

---
+++ examples/readme.js
@@ -0,0 +1,35 @@
+var take = require('../index');
+
+var addNumbers = take('num1', 'num2')(function(reply) {
+
+    /**
+     * All variables are properties of "this"
+     */
+    reply(this.num1 + this.num2);
+
+});
+
+/**
+ * Invoking the Take Function You Just Created
+ */
+
+addNumbers(function(set) {
+
+    /**
+     * Populate the arguments in any order at any time
+     * all arguments have setter methods on "set"
+     */
+    set.num2(40);
+
+    /**
+     * Send the second argument after some time (1s)
+     */
+    setTimeout(function() {
+        set.num1(60);
+    }, 1000);
+
+}).then(function(result) {
+
+    console.log('The sum is:', result);
+
+});
